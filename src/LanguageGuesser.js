0f3dd4b59278fe51acc0590ec26978339cbb37ce
Daniel Nixon <dan.nixon@gmail.com>
2018-01-13 18:36:24 +1100

    Add content-language todo

--- src/LanguageGuesser.js
+++ src/LanguageGuesser.js
@@ -6,6 +6,9 @@
 // see https://github.com/CLD2Owners/cld2
 // see https://dxr.mozilla.org/mozilla-central/source/browser/components/translation/cld2
 exports.guessLanguage = function(htmlLangAttr: ?string, contentLanguage: ?string): ?string {
+
+  // TODO: Handle multiple languages in content-language header. See https://tools.ietf.org/html/rfc7231#section-3.1.3.2
+
   const lang = htmlLangAttr || contentLanguage;
   return lang ? lang.split("-")[0] : undefined;
 }
